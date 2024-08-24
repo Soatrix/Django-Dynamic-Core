3ab11a181e0fa952d2d9d41f594ccc224d230432
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-24 17:53:07 +1200

    Only Allow 1 Default Theme

--- panel/models.py
+++ panel/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
-# Create your models here.
+
 class Theme(models.Model):
     name = models.CharField(max_length=255)
     light_css = models.TextField(blank=True)
     dark_css = models.TextField(blank=True)
+    default = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.default:
+            # Ensure only one Theme can be default
+            Theme.objects.filter(default=True).update(default=False)
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name.title()
