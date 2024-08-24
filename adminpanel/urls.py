0a5bad296afd4a2ca7845ede1318784b87af346c
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-24 21:47:18 +1200

    Add Themes View

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('admin/', AdminDashboardHome.as_view(), name="admin-dashboard-home")
+    path('admin/', AdminDashboardHome.as_view(), name="admin-dashboard-home"),
+    path('admin/themes/', AdminThemesView.as_view(), name="admin-themes"),
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,9 +1,53 @@
 from django.views.generic import TemplateView, View
+from panel.models import Theme
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import IntegrityError
 
 # Create your views here.
-class AdminDashboardHome(TemplateView):
+class AdminDashboardHome(LoginRequiredMixin, TemplateView):
     template_name = "adminpanel/dashboard.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["PAGE_TITLE"] = 'Admin Dashboard'
         return context
+
+class AdminThemesView(LoginRequiredMixin, TemplateView):
+    template_name = "adminpanel/themes.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["PAGE_TITLE"] = "Manage Themes"
+        return context
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        if "create-theme" in request.POST:
+            requiredFields = ["theme-name"]
+            fields = ["theme-name", "theme-light-css", "theme-dark-css"]
+            started = False
+            for field in fields:
+                if field not in request.POST:
+                    if not started:
+                        started = True
+                        context["error"] = "<ul>"
+                    context["error"] = context["error"] + f"<li>{field} is required."
+                elif field in request.POST and field in requiredFields and request.POST.get(field) == "":
+                    if not started:
+                        context["error"] = "<ul>"
+                        started = True
+                    context["error"] = context["error"] + f"<li>The field \"" + field.replace("-", " ").replace("_", " ").title() + "\" is required.</li>"
+            if started:
+                context["error"] = context["error"] + "</ul>"
+            saveRequired = False
+            if not "error" in context:
+                try:
+                    theme = Theme.objects.create(name=request.POST.get("theme-name"), light_css=request.POST.get("theme-light-css"), dark_css=request.POST.get("theme-dark-css"))
+                    saveRequired = True
+                except IntegrityError:
+                    context["success"] = False
+                    context["error"] = "A theme with that name already exists. Please choose a different name."
+
+            if saveRequired:
+                theme.save()
+                context["success"] = True
+        return self.render_to_response(context)
