a68562eb2ce4414355c0aff36131f0d56c522a63
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-24 17:35:27 +1200

    Add Themes

--- panel/admin.py
+++ panel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Theme(models.Model):
+    name = models.CharField(max_length=255)
+    light_css = models.TextField(blank=True)
+    dark_css = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name.title()
