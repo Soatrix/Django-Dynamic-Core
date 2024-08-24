2aa4f352159cb08258d00fc4b6683bbb285b7df3
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-24 20:39:52 +1200

    Setup User Profile

--- panel/models.py
+++ panel/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    theme = models.ForeignKey('Theme', on_delete=models.CASCADE)
+    dark_mode = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
 
 class Theme(models.Model):
     name = models.CharField(max_length=255)
