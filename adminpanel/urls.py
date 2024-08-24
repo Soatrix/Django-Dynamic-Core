1cbbbe65a7e055d64aa1492d0e5565cefeb26380
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-24 22:01:14 +1200

    Add Theme Edit View

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('admin/', AdminDashboardHome.as_view(), name="admin-dashboard-home"),
     path('admin/themes/', AdminThemesView.as_view(), name="admin-themes"),
+    path('admin/themes/edit/<int:id>/', AdminThemeDetailView.as_view(), name='admin-theme-detail'),
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView, View
+from django.shortcuts import get_object_or_404
 from panel.models import Theme
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import IntegrityError
@@ -49,5 +50,43 @@ class AdminThemesView(LoginRequiredMixin, TemplateView):
 
             if saveRequired:
                 theme.save()
+                context["success"] = True
+        return self.render_to_response(context)
+
+class AdminThemeDetailView(LoginRequiredMixin, TemplateView):
+    template_name = "adminpanel/theme-detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["THEME"] = get_object_or_404(Theme, id=self.kwargs["id"])
+        context["PAGE_TITLE"] = "Edit Theme"
+        return context
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        if "update-theme" in request.POST:
+            requiredFields = ["name"]
+            fields = ["name", "light_css", "dark_css"]
+            started = False
+            for field in fields:
+                if field not in request.POST:
+                    if not started:
+                        started = True
+                        context["error"] = "<ul>"
+                    context["error"] = context["error"] + f"<li>{field} is required."
+                elif field in request.POST and field in requiredFields and request.POST.get(field) == "":
+                    if not started:
+                        context["error"] = "<ul>"
+                        started = True
+                    context["error"] = context["error"] + f"<li>The field \"" + field.replace("-", " ").replace("_", " ").title() + "\" is required.</li>"
+            if started:
+                context["error"] = context["error"] + "</ul>"
+            saveRequired = False
+            if not "error" in context:
+                for field in fields:
+                    setattr(context["THEME"], field, request.POST.get(field))
+                    saveRequired = True
+            if saveRequired:
+                context["THEME"].save()
                 context["success"] = True
         return self.render_to_response(context)
